e0c1126c20696db94e28b1dec25e1a21a716031e
serjo96 <promlemboy@gmail.com>
2024-01-27 21:53:04 +0700

    cleanup code;

--- init-postgres.sh
+++ init-postgres.sh
@@ -8,23 +8,23 @@ INIT_DB_SQL="/docker-entrypoint-initdb.d/init-db.sql"
 if [ -n "${POSTGRES_USER}" ] && [ -n "${POSTGRES_DB}" ]; then
 
   until pg_isready --quiet; do
-    echo "Ждем PostgreSQL запустится..."
+    echo "Waiting PostgreSQL running..."
     sleep 1
   done
 
-  # Проверка на существование базы данных
+  # Check existing db
   if [ -z "$(psql -qtAX --dbname="${POSTGRES_DB}" -c "SELECT datname FROM pg_database WHERE datname='${POSTGRES_DB}'")" ]; then
     echo "Creating database: ${POSTGRES_DB}"
     createdb -E UTF8 "${POSTGRES_DB}"
   fi
 
-  # Проверка на существование пользователя базы данных
+  # Check existing user
   if [ -z "$(psql -qtAX --dbname=postgres -c "SELECT usename FROM pg_user WHERE usename='$POSTGRES_USER'")" ]; then
     echo "Creating user: '$POSTGRES_USER'"
     createuser --createdb --login --password "$POSTGRES_USER"
   fi
 
-  # Инициализация базы данных, если инициализационный SQL-скрипт существует
+  # Init db scripts if its exist
   if [ -f "$INIT_DB_SQL" ]; then
     echo "Initializing database"
     psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "POSTGRES_DB" < "$INIT_DB_SQL"
