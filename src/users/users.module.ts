d8469fe0bfa4ab4b22b46ebb2a7fe0d56f0b0931
serjo96 <promlemboy@gmail.com>
2024-02-12 01:11:14 +0700

    Added auth module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,12 +1,16 @@
-import { Module } from '@nestjs/common';
+import {forwardRef, Module} from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { UsersController } from './users.controller';
 import { UserEntity } from './users.entity';
 import { UsersService } from './users.service';
+import {AuthModule} from "~/auth/auth.module";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
+  imports: [
+    TypeOrmModule.forFeature([UserEntity]),
+    forwardRef(() => AuthModule),
+  ],
   providers: [UsersService],
   controllers: [UsersController],
   exports: [UsersService, TypeOrmModule],
